207e994ec605814c03813f9f6c16b62fb23d6b81
Andrew Sidorchuk <sidorchukandrew@gmail.com>
2024-04-03 08:43:47 -0400

    clean up example's App.tsx

--- example/App.tsx
+++ example/App.tsx
@@ -1,11 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native';
-
 import * as Material3Alert from 'material-3-alert';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>{Material3Alert.hello()}</Text>
+      <Pressable onPress={() => Material3Alert.show()}>
+        <Text>Show Alert</Text>
+      </Pressable>
     </View>
   );
 }
